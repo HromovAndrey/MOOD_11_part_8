b9d206d0f99c0d36792b0688de8c7d3c0c80cf8a
user <andrey.gromov.36912@gmail.com>
2024-04-13 23:57:57 +0300

    Лаба 8

--- MOOD_11_part_8/MOOD_11_part_8.py
+++ MOOD_11_part_8/MOOD_11_part_8.py
@@ -1,1 +1,50 @@
-class Import
+class MyMeta(type):
+
+    def __new__(cls, name, bases, dct):
+         print(dct)
+        return super(). __new__ (cls, name, bases, dct)
+    @staticmethod
+    def cache_decor(method):
+        def decor_method(*args, **kwargs):
+            cache_data = (method.__name__, *args, tuple(kwargs.items)
+
+
+            if cache_data in MyMeta.cache:
+                print("known data")
+                return MyMeta.cache[cache_data]
+            result = method
+
+class Number:
+   value = 10
+
+    def __add__(self, ):
+    def __mul__(self, other):
+        return self.value - other.value
+
+
+class InfoSystem:
+    def add(self, num1, num2):
+        return num1 + num2
+
+
+obj =   InfoSystem()
+print(InfoSystem.add (2,2))
+print(InfoSystem.add (2,2))
+print(InfoSystem.add (2,2))
+print(InfoSystem.add (2,2))
+for date, result in MyMeta.cache.items():
+    print(data, result)
+
+class MyMeta(types):
+    cache = {}
+
+    def __new__(cls, name, bases, dct):
+
+
+
+
+
+
+
+
+
