e89eafd7422e4bbcbf35defbf4d690c0072489f6
user <andrey.gromov.36912@gmail.com>
2024-04-14 00:11:25 +0300

    Лаба 8

--- laba_mood_11_part_8.py
+++ laba_mood_11_part_8.py
@@ -1,1 +1,17 @@
-Class Person:
+# Завдання 1
+# Задайте метаклас, що автоматично додає
+# додатковий функціонал до всіх класів, що його
+# використовують.
+class MyMeta(type):
+    def __new__(cls, name, bases, dct):
+        dct['additional_functionality'] = lambda self: print("Additional functionality added!")
+        return super().__new__(cls, name, bases, dct)
+class MyMeta(metaclass=MyMeta):
+    pass
+
+obj = MyMeta()
+obj.additional_functionality()
+
+
+
+
