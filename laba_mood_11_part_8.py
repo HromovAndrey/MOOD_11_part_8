668e06538010a67d1f27c9847bb6da3f2fde4a76
user <andrey.gromov.36912@gmail.com>
2024-04-14 13:21:38 +0300

    Лаба 8 робота 2

--- laba_mood_11_part_8.py
+++ laba_mood_11_part_8.py
@@ -1,16 +1,27 @@
-# Завдання 1
-# Задайте метаклас, що автоматично додає
-# додатковий функціонал до всіх класів, що його
-# використовують.
-class MyMeta(type):
-    def __new__(cls, name, bases, dct):
-        dct['additional_functionality'] = lambda self: print("Additional functionality added!")
-        return super().__new__(cls, name, bases, dct)
-class MyMeta(metaclass=MyMeta):
-    pass
+# Завдання 2
+# Створіть метаклас, що перевіряє наявність певних
+# атрибутів у всіх класах, які використовують цей
+# метаклас.
+class MetaClass2(type):
+    def __init__(cls, name, bases, dct):
+        if 'required_attribute' not in dct:
+            raise TypeError(f"Class {name} must have a 'required_attribute' attribute")
+        super().__init__(name, bases, dct)
 
-obj = MyMeta()
-obj.additional_functionality()
+
+class Person(metaclass=MetaClass2):
+    age = 20
+    name = "Mary"
+
+    def info(self):
+        print(f"Name:{self.name}, age {self.age} ")
+
+
+
+
+
+
+
 
 
 
