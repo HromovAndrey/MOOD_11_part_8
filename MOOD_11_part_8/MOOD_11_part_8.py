80d7d79bea0b2d29b2c7966562230d135fac54fa
user <andrey.gromov.36912@gmail.com>
2024-04-14 20:42:16 +0300

    Лаба 8

--- MOOD_11_part_8/MOOD_11_part_8.py
+++ MOOD_11_part_8/MOOD_11_part_8.py
@@ -1,47 +1,102 @@
+# class MyMeta(type):
+#     def __new__(cls, name, bases, dct):
+#         print(f"{cls=}")
+#         print(f"{name=}")
+#         print(f"{bases=}")
+#         print(f"{dct=}")
+#         return super(). __new__(cls, name, bases, dct)
+#
+# class Perenta:
+#     country = "Ukraine"
+#
+# class Person(Perenta, metaclass=MyMeta):
+#     age = 20
+#     name="Mary"
+#
+#
+#     def __init__(self, country="Ukraine"):
+#         self.country = country
+#
+#     def info(self):
+#         print(f"Name : {self.name}, age:{self.age}")
+#
+#
+# class MyMeta(type):
+#     def __new__(cls, name, bases, dct):
+#         dct["country"] = "Ukraine"
+#         if "name" not in dct:
+#             raise AttributeError("There is no attribute name")
+#         return super().__new__(cls, name, bases, dct)
+# class Person( metaclass=MyMeta):
+#     age = 20
+#     name = "Mary"
+#
+#     def __init__(self, country="Ukraine"):
+#         self.country = country
+#
+#     def info(self):
+#         print(f"Name : {self.name}, age:{self.age}")
+#
+# print(Person.country)
+
+# class MyMeta(type):
+#     needed_methods = ["info" "get"]
+#     def __init__(cls, name, bases, dct):
+#         for method_name in cls.needed_methods:
+#             if method_name not in dct:
+#                 raise AttributeError(f"There is not method {method_name} in class {name}")
+#         return super(). __new__(cls, name, bases, dct)
+#
+# class Person(metaclass=MyMeta):
+#     age = 20
+#     name = "Mary"
+#
+#     def info(self):
+#         print(f"Name:{self.name}, age {self.age} ")
+#
+#     def get
+
+
 class MyMeta(type):
-
     def __new__(cls, name, bases, dct):
-         print(dct)
-        return super(). __new__ (cls, name, bases, dct)
+        for method_name, method in dct.items():
+            if callable(method):
+                dct[method_name] = cls.meta_decor(method)
+
+        return super(). __new__(cls, name, bases, dct)
     @staticmethod
-    def cache_decor(method):
-        def decor_method(*args, **kwargs):
-            cache_data = (method.__name__, *args, tuple(kwargs.items)
+    def meta_decor(method):
+        def decored_method(*args, **kwargs):
+            print("hello from metaclass")
+            return method(*args, **kwargs)
+
+        return decored_method()
 
 
-            if cache_data in MyMeta.cache:
-                print("known data")
-                return MyMeta.cache[cache_data]
-            result = method
+class Number(metaclass=MyMeta):
+    value = 10
 
-class Number:
-   value = 10
+    def __add__(self, other):
+        return self.value + other.value
 
-    def __add__(self, ):
+
     def __mul__(self, other):
+        return self.value * other.value
+
+
+    def __sub__(self, other):
         return self.value - other.value
 
-
-class InfoSystem:
-    def add(self, num1, num2):
-        return num1 + num2
+    def __truediv__(self, other):
+        return self.value / other.value
 
 
-obj =   InfoSystem()
-print(InfoSystem.add (2,2))
-print(InfoSystem.add (2,2))
-print(InfoSystem.add (2,2))
-print(InfoSystem.add (2,2))
-for date, result in MyMeta.cache.items():
-    print(data, result)
-
-class MyMeta(types):
-    cache = {}
-
-    def __new__(cls, name, bases, dct):
-
-
-
+num1 = Number()
+num2 = Number()
+print(num1 + num2)
+print(num1 * num2)
+print(num1 - num2)
+print(num1 / num2)
 
 
 
